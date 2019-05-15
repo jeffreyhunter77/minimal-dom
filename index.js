367d3cf109b555b5aa6d224e6b35f81226727d56
Jeff Hunter <jeff@missioncriticallabs.com>
2019-05-15 09:56:33 -0400

    provide direct access to the Node and DOMException classes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var Document = require('./lib/document');
 
+Document.Node = require('./lib/node');
+Document.DOMException = require('./lib/dom-exception');
+
 module.exports = Document;
--- tests/index-test.js
+++ tests/index-test.js
@@ -2,12 +2,22 @@ require('./setup');
 
 var main = require('../')
   , Document = require('../lib/document')
+  , Node = require('../lib/node')
+  , DOMException = require('../lib/dom-exception')
 ;
 
 describe(main, () => {
 
   it('exports Document', function() {
     expect(main).to.equal(Document);
+  });
+
+  it('provides access to the Node class', function() {
+    expect(main.Node).to.equal(Node);
+  });
+
+  it('provides access to the DOMException class', function() {
+    expect(main.DOMException).to.equal(DOMException);
   });
 
 });
